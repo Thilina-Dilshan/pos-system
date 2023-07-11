2db9f4947038cc142d475a3c4c9c2be66cb0a898
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-11 23:18:51 +0530

    Finalize Customer-Controller

--- back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
@@ -1,10 +1,17 @@
 package lk.ijse.dep10.pos;
 
+import lk.ijse.dep10.pos.api.CustomerController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 public class WebAppConfig {
+
+    @Bean
+    public CustomerController customerController(){
+        return new CustomerController();
+    }
 
 }
