5dd111b73ac8682e00f6b4a4f872dd5166b4d74b
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-12 00:27:39 +0530

    Add Order-Controller

--- back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep10.pos;
 
 import lk.ijse.dep10.pos.api.CustomerController;
+import lk.ijse.dep10.pos.api.ItemController;
+import lk.ijse.dep10.pos.api.OrderController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,6 +14,16 @@ public class WebAppConfig {
     @Bean
     public CustomerController customerController(){
         return new CustomerController();
+    }
+
+    @Bean
+    public ItemController itemController(){
+        return new ItemController();
+    }
+
+    @Bean
+    public OrderController orderController() {
+        return new OrderController();
     }
 
 }
--- back-end/src/main/java/lk/ijse/dep10/pos/api/ItemController.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/api/ItemController.java
@@ -1,12 +1,99 @@
 package lk.ijse.dep10.pos.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.dep10.pos.dto.ItemDTO;
+import lk.ijse.dep10.pos.dto.ResponseErrorDTO;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/items")
 public class ItemController {
 
+    @Autowired
+    private BasicDataSource pool;
+
+    @PostMapping
+    public ResponseEntity<?> saveItem (@RequestBody ItemDTO item) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO item(code,description, qty, unit_price) VALUES (?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            System.out.println(item.getDescription() + item.getQty() + item.getUnitPrice());
+            stm.setString(1, item.getCode());
+            stm.setString(2, item.getDescription());
+            stm.setInt(3, Integer.parseInt(item.getQty()+""));
+            stm.setBigDecimal(4, new BigDecimal(item.getUnitPrice()+""));
+            stm.executeUpdate();
+            return new ResponseEntity<>(item, HttpStatus.CREATED);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(
+                    new ResponseErrorDTO(500, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getItems( @RequestParam(value = "q",required = false) String query) {
+        if (query == null) query = "";
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection
+                    .prepareStatement
+                            ("SELECT * FROM item WHERE code LIKE ? OR description LIKE ? OR qty LIKE ? OR item.unit_price LIKE ?");
+            query = "%" + query + "%";
+
+            for (int i = 1; i <= 4; i++) {
+                stm.setString(i, query);
+            }
+
+            ResultSet rst = stm.executeQuery();
+            ArrayList<ItemDTO> itemList = new ArrayList<>();
+            while (rst.next()) {
+                String code = rst.getString("code");
+                String description = rst.getString("description");
+                int qty = rst.getInt("qty");
+                BigDecimal unitPrice = rst.getBigDecimal("unit_price");
+                itemList.add(new ItemDTO(code, description, qty, unitPrice));
+            }
+            return new ResponseEntity<>(itemList, HttpStatus.OK);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(new ResponseErrorDTO(500, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<?> getItem(@PathVariable String code) {
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM item WHERE code=?");
+            stm.setString(1, code);
+            ResultSet rst = stm.executeQuery();
+            if (rst.next()){
+                String description = rst.getString("description");
+                int qty = rst.getInt("qty");
+                BigDecimal unitPrice = rst.getBigDecimal("unit_price").setScale(2);
+                ItemDTO item = new ItemDTO(code, description, qty, unitPrice);
+                return new ResponseEntity<>(item, HttpStatus.OK);
+            }else {
+                ResponseErrorDTO error = new ResponseErrorDTO(404, "Item not found");
+                return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            ResponseErrorDTO error = new ResponseErrorDTO(500, "Failed to fetch the item, try again!");
+            return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
