86f09dab1cd510859ef9ff9e93a96fdd21158c3a
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-11 22:26:35 +0530

    Add Customer-Controller

---
+++ back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerController.java
@@ -0,0 +1,12 @@
+package lk.ijse.dep10.pos.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+@CrossOrigin
+public class CustomerController {
+
+}
