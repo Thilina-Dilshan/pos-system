7ca6dde9d8db9d0dd7619917255555c0829d4309
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-17 20:50:11 +0530

    Implement ItemDAO

--- back-end/src/main/java/lk/ijse/dep10/pos/dao/util/Mappers.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/dao/util/Mappers.java
@@ -1,6 +1,9 @@
 package lk.ijse.dep10.pos.dao.util;
 
 import lk.ijse.dep10.pos.entity.Customer;
+import lk.ijse.dep10.pos.entity.Item;
+
+import java.math.BigDecimal;
 
 public class Mappers {
     public static final RowMapper<Customer> CUSTOMER_ROW_MAPPER = ((rs, rowNum) -> {
@@ -10,5 +13,13 @@ public class Mappers {
         String contact = rs.getString("contact");
         return new Customer(id, name, address, contact);
     });
+
+    public static final RowMapper<Item> ITEM_ROW_MAPPER = (rs, rowNum) -> {
+        String code = rs.getString("code");
+        String description = rs.getString("description");
+        int qty = rs.getInt("qty");
+        BigDecimal unitPrice = rs.getBigDecimal("unit_price");
+        return new Item(code, description, qty, unitPrice);
+    };
 
 }
