d18b2af3b4148f6784b14311b2264ad3864c05de
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-18 12:00:58 +0530

    Test DAOFactory

---
+++ back-end/src/test/java/lk/ijse/dep10/pos/dao/DAOFactoryTest.java
@@ -0,0 +1,33 @@
+package lk.ijse.dep10.pos.dao;
+
+import lk.ijse.dep10.pos.dao.custom.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DAOFactoryTest {
+
+    @Test
+    void getInstance() {
+        DAOFactory instance1 = DAOFactory.getInstance();
+        DAOFactory instance2 = DAOFactory.getInstance();
+
+        assertEquals(instance1,instance2);
+    }
+
+    @Test
+    void getDAO() {
+        CustomerDAO customerDAO = DAOFactory.getInstance().getDAO(DAOType.CUSTOMER);
+        ItemDAO itemDAO = DAOFactory.getInstance().getDAO(DAOType.ITEM);
+        OrderDAO orderDAO = DAOFactory.getInstance().getDAO(DAOType.ORDER);
+        OrderDetailDAO orderDetailDAO = DAOFactory.getInstance().getDAO(DAOType.ORDER_DETAIL);
+        OrderCustomerDAO orderCustomerDAO = DAOFactory.getInstance().getDAO(DAOType.ORDER_CUSTOMER);
+
+        /* Verify */
+        assertNotNull(customerDAO);
+        assertNotNull(itemDAO);
+        assertNotNull(orderDAO);
+        assertNotNull(orderDetailDAO);
+        assertNotNull(orderCustomerDAO);
+    }
+}
