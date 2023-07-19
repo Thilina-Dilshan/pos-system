aebb07f5292bef1fab51cf8a92b8585171676bf6
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-19 12:50:46 +0530

    Update WSHandler

--- back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerWSHandler.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerWSHandler.java
@@ -1,6 +1,11 @@
 package lk.ijse.dep10.pos.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lk.ijse.dep10.pos.business.BOFactory;
+import lk.ijse.dep10.pos.business.BOType;
+import lk.ijse.dep10.pos.business.custom.CustomerBO;
+import lk.ijse.dep10.pos.business.exception.BusinessException;
+import lk.ijse.dep10.pos.business.exception.BusinessExceptionType;
 import lk.ijse.dep10.pos.dto.CustomerDTO;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,22 +26,14 @@ public class CustomerWSHandler extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        try (Connection connection = pool.getConnection()) {
-            System.out.println("Websocket handler");
-            PreparedStatement stm = connection
-                    .prepareStatement("SELECT * FROM customer WHERE id=? OR contact=?");
-            stm.setString(1, message.getPayload().strip());
-            stm.setString(2, message.getPayload().strip());
-            ResultSet resultSet = stm.executeQuery();
-            if (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String address = resultSet.getString("address");
-                String contact = resultSet.getString("contact");
-                CustomerDTO customer = new CustomerDTO(id, name, address, contact);
-                String customerJSON = objectMapper.writeValueAsString(customer);
-                session.sendMessage(new TextMessage(customerJSON));
-            }
+        CustomerBO customerBO = BOFactory.getInstance().getBO(BOType.CUSTOMER, pool);
+        try {
+            CustomerDTO customer = customerBO
+                    .findCustomerByIdOrContact(message.getPayload().strip());
+            String customerJSON = objectMapper.writeValueAsString(customer);
+            session.sendMessage(new TextMessage(customerJSON));
+        } catch (BusinessException be) {
+            if (be.getType() != BusinessExceptionType.RECORD_NOT_FOUND) throw be;
         }
     }
 }
--- back-end/src/main/java/lk/ijse/dep10/pos/api/OrderController.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/api/OrderController.java
@@ -2,9 +2,7 @@ package lk.ijse.dep10.pos.api;
 
 import lk.ijse.dep10.pos.business.BOFactory;
 import lk.ijse.dep10.pos.business.BOType;
-import lk.ijse.dep10.pos.business.SuperBO;
 import lk.ijse.dep10.pos.business.custom.OrderBO;
-import lk.ijse.dep10.pos.dto.ItemDTO;
 import lk.ijse.dep10.pos.dto.OrderDTO;
 import lk.ijse.dep10.pos.dto.OrderDTO2;
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -12,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.*;
 import java.util.List;
 
 @RequestMapping("api/v1/orders")
@@ -25,15 +22,15 @@ public class OrderController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public Integer saveOrder(@RequestBody OrderDTO order) throws Exception{
+    public Integer saveOrder(@RequestBody OrderDTO order) throws Exception {
         OrderBO orderBO = BOFactory.getInstance().getBO(BOType.ORDER, pool);
         return orderBO.placeOrder(order);
     }
 
 
     @GetMapping
-    public List<OrderDTO2> getOrders (@RequestParam(value = "q", required = false) String query) throws Exception {
-        if(query==null) query = "";
+    public List<OrderDTO2> getOrders(@RequestParam(value = "q", required = false) String query) throws Exception {
+        if (query == null) query = "";
         OrderBO orderBO = BOFactory.getInstance().getBO(BOType.ORDER, pool);
         return orderBO.searchOrders(query);
 
