b0f17320845146c0062f392ec11504f69d977394
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-19 21:04:58 +0530

    Finalize Monolithic integration

--- front-end/js/place-order.js
+++ front-end/js/place-order.js
@@ -7,7 +7,7 @@ import {getBillDesignHTML} from "./bill-design.js";
 /* Module Level Variables, Constants */
 
 const REST_API_BASE_URL = 'http://localhost:8080/pos/api/v1';
-const WS_API_BASE_URL = 'ws://localhost:8080/pos';
+const WS_API_BASE_URL = 'ws://localhost:8080/pos/api/v1';
 const orderDateTimeElm = $("#order-date-time");
 const tbodyElm = $("#tbl-order tbody");
 const tFootElm = $("#tbl-order tfoot");
