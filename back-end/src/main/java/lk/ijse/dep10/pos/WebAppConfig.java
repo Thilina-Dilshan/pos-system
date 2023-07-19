62333e5a9f0acb51610e2af2834cbbf40ad5175c
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-19 18:13:16 +0530

    Add methodValidationPostProcessor

--- back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
@@ -5,11 +5,16 @@ import lk.ijse.dep10.pos.api.ItemController;
 import lk.ijse.dep10.pos.api.OrderController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 public class WebAppConfig {
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor(){
+        return new MethodValidationPostProcessor();
+    }
 
     @Bean
     public CustomerController customerController(){
